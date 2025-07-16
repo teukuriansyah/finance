1b548077f791195a0d9196104950d0a4078a0846
teukuriansyah <128889699+teukuriansyah@users.noreply.github.com>
2025-07-16 17:39:37 +0700

    refactor: add notes and date

--- server/dto/TransactionDto.go
+++ server/dto/TransactionDto.go
@@ -4,6 +4,8 @@ import ("gorm.io/gorm")
 type Transaction struct {
   gorm.Model
   IdUser string `json:"idUser" gorm:"not null"` 
+  Date string `json:"date" gorm:"not null"`
+  Notes string `json:"notes" gorm:"not null"`
   Amount int64 `json:"amount" gorm:"not null"`
   IsIncrease bool `json:"isIncrease" gorm:"not null"`
 }
